036fe6c90dd043348d904b08b9956d73e45b14e8
branko.kokanovic <branko.kokanovic@gmail.com>
2014-11-08 23:45:16 +0100

    just so people not hit real server accidentally

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ STACK_EXCHANGE_APP_KEY = '<insert-app-key>'
 MINIMAL_DATETIME_UTC = Delorean(datetime.datetime(2006, 1, 1), 'UTC').datetime
 MAXIMUM_DATETIME_UTC = Delorean(datetime.datetime(2010, 1, 1), 'UTC').datetime
 
-MONGO_SERVER = 'challmon.kokanovic.org'
-MONGO_PORT = 26016
+MONGO_SERVER = 'localhost'
+MONGO_PORT = 27017
